bf38695b065d17029a49193d6080586371f0ab00
Tomás Simonetti <tomysimonetti3@gmail.com>
2023-08-26 19:34:51 -0300

    Added icons to $ items and adjusted styling

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { ref, watchEffect, computed, onMounted } from 'vue'
+import DollarIcon from '@/components/icons/DollarIcon.vue'
+import EuroIcon from '@/components/icons/EuroIcon.vue'
 // fetch data from https://dolar-api-argentina.vercel.app/v1/dolares
 // const response = await fetch('https://api.bluelytics.com.ar/v2/latest')
 
@@ -8,10 +10,26 @@ const data = ref({})
 const date = ref(new Date())
 const formatedDate = ref('')
 const currencyNames = {
-    oficial: 'Dólar Oficial',
-    blue: 'Dólar Blue',
-    oficial_euro: 'Euro Oficial',
-    blue_euro: 'Euro Blue',
+    oficial: {
+        name: 'Dólar Oficial',
+        icon: DollarIcon,
+        color: 'var(--accent-dark-1)'
+    },
+    blue: {
+        name: 'Dólar Blue',
+        icon: DollarIcon,
+        color: 'var(--blue)'
+    },
+    oficial_euro: {
+        name: 'Euro Oficial',
+        icon: EuroIcon,
+        color: 'var(--accent-dark-1)'
+    },
+    blue_euro: {
+        name: 'Euro Blue',
+        icon: EuroIcon,
+        color: 'var(--blue)'
+    }
 }
 // Consults API DB
 const divisasUpdate = async () => {
@@ -48,7 +66,10 @@ const formatSpanishDate = computed(() => {
   <main>
     <div class="divisas__wrapper">
       <div v-for="(divisa, currencyCode) in divisas" :key="currencyCode" class="divisas__item">
-        <h3>{{ currencyNames[currencyCode] }}</h3>
+        <div class="divisas__title">
+          <component class="divisas__icon" :is="currencyNames[currencyCode].icon" :style="{ color: currencyNames[currencyCode].color }"/>
+          <h3>{{ currencyNames[currencyCode].name }}</h3>
+        </div>
         <div class="divisas__buy">
           <label>Compra:</label>
           <p class="divisas__number">${{ divisa.value_buy }}</p>
@@ -77,40 +98,53 @@ const formatSpanishDate = computed(() => {
     max-width: 720px
     width: 95%
     margin: 0 auto
-    @media (max-width: 570px)
+    @media (max-width: 700px)
         grid-template-columns: 1fr
         grid-template-rows: repeat(4, 1fr)
         grid-column-gap: 0
         grid-row-gap: 1rem
+        justify-items: center
         max-width: 720px
         width: 90%
         margin: 0 auto
+        .divisas__item
+            max-width: 500px
+            width: 100%
     .divisas__item
-        display: grid
-        grid-template-columns: 2fr
-        grid-template-rows: 1fr 1fr 1fr
-        grid-row-gap: 10px
+        display: flex
+        flex-flow: column nowrap
+        gap: 1rem
         border: 1px solid lighten(#000, 70%)
         border-bottom: 4px solid lighten(#000, 70%)
         border-radius: 1rem
-        line-height: 0.9
+        line-height: 1
         padding: 1rem
         background-color: var(--white)
         box-shadow: 0.2rem 0.3rem 0.8rem rgba(0,0,0,0.2)
         transition: all 0.3s ease
-        
         &:hover
-          border-color: var(--accent-dark-1)
-        h3
-            font-size: 2rem
+            border-color: var(--accent-dark-1)
+
+        .divisas__title
+            display: flex
+            flex-direction: row
+            align-items: center
+            gap: 1rem
+            font-size: var(--font2)
             margin-bottom: 1rem
+        .divisas__icon
+            width: 2rem
+            aspect-ratio: 1/1
+            max-width: 100px
+            max-height: 100px
+            display: block
         .divisas__buy, .divisas__sell
             display: flex
             justify-content: space-between
             width: 100%
         label
             font-weight: 300
-            font-size: 1.4rem
+            font-size: var(--font0)
         .divisas__number
             font-weight: 600
             font-size: 2rem
@@ -127,7 +161,7 @@ const formatSpanishDate = computed(() => {
                 font-size: 1.5rem
 .last-update
     font-size: 1.2rem
-    margin-top: 1rem
+    margin-block: 1rem 2rem
     text-align: center
     line-height: 1
     color: var(--accent-dark-3)
@@ -135,5 +169,4 @@ const formatSpanishDate = computed(() => {
         font-size: 0.8rem
         font-weight: 300
         color: var(--black)
-    p:last-child
 </style>
